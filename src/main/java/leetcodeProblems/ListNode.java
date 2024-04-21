ea38e78149a1e73875a9984f2adaf0ddff51a1c3
Vaishaliyadavv <vaishali.yadav09@gmail.com>
2024-04-21 15:46:40 +0530

    add ListNode.java

---
+++ src/main/java/leetcodeProblems/ListNode.java
@@ -0,0 +1,19 @@
+package leetcodeProblems;
+
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/1196623028/
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
